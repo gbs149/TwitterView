9d9f509ad504a427b846728cc1cb9b9ef4a54a19
gabriel <gabrielbs@dbserver.com.br>
2020-02-10 22:58:32 -0300

    Spring Boot Starter

---
+++ src/main/java/com/gbs149/twitterview/TwitterviewApplication.java
@@ -0,0 +1,13 @@
+package com.gbs149.twitterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterviewApplication.class, args);
+	}
+
+}
